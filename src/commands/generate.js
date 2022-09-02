5a850670c5cdc78066ec5c87720a36f6ba9f30c6
eliasassuncao <elias3748@gmail.com>
2022-09-02 19:27:39 -0300

    feat(form): create formatted form by asks

--- src/commands/generate.js
+++ src/commands/generate.js
@@ -8,7 +8,8 @@ module.exports = {
       parameters,
       template: { generate },
       print,
-      prompt
+      prompt,
+      filesystem
     } = toolbox
 
     const name = parameters.first
@@ -17,40 +18,56 @@ module.exports = {
     const singleName = `${formattedName}Single`
     const listName = `${formattedName}List`
     const createName = `${formattedName}Create`
-    const formName = `${formattedName}Form`
     const editName = `${formattedName}Edit`
 
-    const currentPath = toolbox.filesystem.cwd()
+    const currentPath = filesystem.cwd()
+    const generateAll = await prompt.confirm('Deseja gerar o CRUD completo?')
 
+    const result = generateAll ? 
+      { 
+        list: true,
+        form: 'all',
+        single: true
+      }
+      :
+      await prompt.ask([
+        {
+          type: 'confirm',
+          name: 'list',
+          message: 'Gerar pagina de listagem?',
+          initial: true
+        },
+        {
+          type: 'confirm',
+          name: 'single',
+          message: 'Gerar pagina de visualização?',
+          initial: true
+        },
+        {
+          type: 'select',
+          name: 'form',
+          message: 'Como deseja gerar o formulario?',
+          choices: ['all', 'create', 'replace'],
+          initial: true
+        }
+      ])
+
+    const formNames = {
+      all: 'Form',
+      create: 'Create',
+      replace: 'Edit'
+    }
+    const formName = `${formattedName}${formNames[result.form]}`
     const crudPaths = {
       list: `${currentPath}/src/pages/${name}/${formattedName}List.vue`,
-      form: `${currentPath}/src/pages/${name}/${formattedName}Form.vue`,
+      form: `${currentPath}/src/pages/${name}/${formName}.vue`,
       single: `${currentPath}/src/pages/${name}/${formattedName}Single.vue`,
     }
 
-    const result = await prompt.ask([
-      {
-        type: 'confirm',
-        name: 'list',
-        message: 'Gerar pagina de listagem?',
-        initial: true
-      },
-      {
-        type: 'confirm',
-        name: 'form',
-        message: 'Gerar pagina de formulario?',
-        initial: true
-      },
-      {
-        type: 'confirm',
-        name: 'single',
-        message: 'Gerar pagina de visualização?',
-        initial: true
-      }
-    ])
-
     for (const key in result) {
       if (!result[key]) continue
+
+      const isForm = key === 'form'
 
       // Gerar página
       await generate({
@@ -62,14 +79,24 @@ module.exports = {
           listName,
           createName,
           formName,
-          editName
+          editName,
+          ...(isForm && { type: result[key] })
         },
         target: crudPaths[key]
       })
     }
 
-    // Gerar rota
     const camelCaseName = strings.camelCase(name)
+
+    // const routeFile = toolbox.filesystem.read(`${currentPath}/src/router/routes.js`)
+    // console.log(routeFile, '<-- routeFile')
+    // console.log(`import ${camelCaseName} from './modules/${camelCaseName}'\n${routeFile}`)
+    // toolbox.filesystem.write(
+    //   `${currentPath}/src/router/routes.js`,
+    //   `import ${camelCaseName} from './modules/${camelCaseName}'\n${routeFile}`
+    // )
+
+    // Gerar rota
     await generate({
       template: 'route.js.ejs',
       target: `${currentPath}/src/router/modules/${camelCaseName}.js`,
@@ -87,8 +114,6 @@ module.exports = {
       props: { name }
     })
 
-    print.debug(result)
-
-    print.info(`Generated file at models/${name}-model.js`)
+    print.success(`Model ${name} gerado com sucesso!`)
   }
 }
