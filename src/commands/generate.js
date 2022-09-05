fb62397ca6353e306b1734dca47a568f67423463
eliasassuncao <elias3748@gmail.com>
2022-09-05 17:28:25 -0300

    refactor: refactor code

--- src/commands/generate.js
+++ src/commands/generate.js
@@ -19,74 +19,63 @@ module.exports = {
     const listName = `${formattedName}List`
     const createName = `${formattedName}Create`
     const editName = `${formattedName}Edit`
-
     const currentPath = filesystem.cwd()
-    const generateAll = await prompt.confirm('Deseja gerar o CRUD completo?', true)
-
-    const result = generateAll ? 
-      { 
-        list: true,
-        form: 'all',
-        single: true
-      }
-      :
-      await prompt.ask([
-        {
-          type: 'confirm',
-          name: 'list',
-          message: 'Gerar pagina de listagem?',
-          initial: true
-        },
-        {
-          type: 'confirm',
-          name: 'single',
-          message: 'Gerar pagina de visualização?',
-          initial: true
-        },
-        {
-          type: 'select',
-          name: 'form',
-          message: 'Como deseja gerar o formulario?',
-          choices: ['all', 'create', 'replace', 'not-create'],
-          initial: 'all'
-        }
-      ])
-
     const formName = `${formattedName}Form`
     const crudPaths = {
       list: `${currentPath}/src/pages/${name}/${formattedName}List.vue`,
       form: `${currentPath}/src/pages/${name}/${formName}.vue`,
       single: `${currentPath}/src/pages/${name}/${formattedName}Single.vue`,
     }
-
-    for (const key in result) {
-      const isForm = key === 'form'
-
-      if (!result[key] || (isForm && result[key] === 'not-create')) continue
-
-      // Gerar página
-      await generate({
-        template: `${key}.vue.ejs`,
-        props: {
-          name,
-          formattedName,
-          singleName,
-          listName,
-          createName,
-          formName,
-          editName,
-          ...(isForm && { type: result[key] })
-        },
-        target: crudPaths[key]
-      })
-    }
-
     const camelCaseName = strings.camelCase(name)
+    const generateAll = await prompt.confirm('Deseja gerar o CRUD completo?', true)
+    const result = generateAll ? 
+    { 
+      list: true,
+      form: 'all',
+      single: true
+    }
+    :
+    await prompt.ask([
+      {
+        type: 'confirm',
+        name: 'list',
+        message: 'Gerar pagina de listagem?',
+        initial: true
+      },
+      {
+        type: 'confirm',
+        name: 'single',
+        message: 'Gerar pagina de visualização?',
+        initial: true
+      },
+      {
+        type: 'select',
+        name: 'form',
+        message: 'Como deseja gerar o formulario?',
+        choices: ['all', 'create', 'replace', 'not-create'],
+        initial: 'all'
+      }
+    ])
 
-    // Gerar rota
-    await generate({
-      template: 'route.js.ejs',
-      target: `${currentPath}/src/router/modules/${camelCaseName}.js`,
+    await generatePages({
+      result,
+      props: {
+        name,
+        formattedName,
+        singleName,
+        listName,
+        createName,
+        formName,
+        editName,
+      },
+      crudPaths,
+      generate
+    })
+
+    await generateRoutes({
+      generate,
+      currentPath,
+      camelCaseName,
       props: {
         name,
         formattedName,
@@ -94,23 +83,57 @@ module.exports = {
       }
     })
 
-
-    const responseStore = await prompt.ask({
-      type: 'select',
-      name: 'store',
-      message: 'Qual tipo de store você deseja?',
-      choices: ['pinia', 'vuex'],
-      initial: 'pinia'
-    })
-
-    // Gerar store
-    await generate({
-      template: `${responseStore.store}.js.ejs`,
-      target: `${currentPath}/src/store/modules/${camelCaseName}.js`,
-      props: { name }
+    await generateStore({
+      generate,
+      currentPath,
+      camelCaseName,
+      name,
+      prompt
     })
 
     print.success(`Model ${name} gerado com sucesso!`)
     print.info('Lembre-se de importar os arquivos de rota e store gerados.')
   }
+}
+
+async function generatePages ({ result, props, crudPaths, generate }) {
+  for (const key in result) {
+    const isForm = key === 'form'
+
+    if (!result[key] || (isForm && result[key] === 'not-create')) continue
+
+    // Gerar página
+    await generate({
+      template: `${key}.vue.ejs`,
+      props: {
+        ...props,
+        ...(isForm && { type: result[key] })
+      },
+      target: crudPaths[key]
+    })
+  }
+}
+
+async function generateRoutes ({ generate, currentPath, camelCaseName, props }) {
+  await generate({
+    template: 'route.js.ejs',
+    target: `${currentPath}/src/router/modules/${camelCaseName}.js`,
+    props
+  })
+}
+
+async function generateStore ({ currentPath, camelCaseName, name, prompt, generate }) {
+  const responseStore = await prompt.ask({
+    type: 'select',
+    name: 'store',
+    message: 'Qual tipo de store você deseja?',
+    choices: ['pinia', 'vuex'],
+    initial: 'pinia'
+  })
+
+  await generate({
+    template: `${responseStore.store}.js.ejs`,
+    target: `${currentPath}/src/store/modules/${camelCaseName}.js`,
+    props: { name }
+  })
 }
